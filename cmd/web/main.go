5ed60b6eedde1b10c02b1a65ae8cfeecf5c89540
Pinglei He <pingleihe@outlook.com>
2023-02-13 12:57:40 +0100

    add leveled loggin

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// parse address from command line
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	// custom logger for leveled logging
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERRO\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -23,7 +28,7 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// start the server
-	log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
