4c6ccb651389fee14612148ef783ccf185f4fdde
Pinglei He <pingleihe@outlook.com>
2023-02-13 15:33:51 +0100

    added database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,38 +17,46 @@ type application struct {
 func main() {
 	// parse address from command line
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// custom logger for leveled logging
 	infoLog := log.New(os.Stdout, "INFO \t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR \t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	infoLog.Println("Successfully established connection to database")
+	defer db.Close()
+
 	app := &application{
 		infoLog:  infoLog,
 		errorLog: errorLog,
 	}
 
-	mux := http.NewServeMux()
-
-	// server static files
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// configure handlers
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// server configuration
 	// ErrorLog is added so that the server writes its errors to stderr
 	srv := &http.Server{
-		Handler:  mux,
+		Handler:  app.routes(),
 		Addr:     *addr,
 		ErrorLog: app.errorLog,
 	}
 
 	// start the server
 	app.infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, err
 }
